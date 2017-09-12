d90a2e8c7cfc31bf77451d6113c767f4ee5f7c20
neinzz <heins.nicholas@gmail.com>
2017-09-11 21:44:37 -0700

    Initial commit

---
+++ Source/ProjectToba/ProjectTobaGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectTobaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTTOBA_API AProjectTobaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
